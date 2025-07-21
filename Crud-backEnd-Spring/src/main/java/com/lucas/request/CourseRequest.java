be19fe94768afa0be020420eeb86f9a8810b6adc
Lucas Araujo <assuncao.araujo.lucas@gmail.com>
2025-07-21 18:28:48 -0300

    feat: adicionar anotação ValueOfEnum e validadores para categorias em CourseRequest e CourseResponse

--- Crud-backEnd-Spring/src/main/java/com/lucas/request/CourseRequest.java
+++ Crud-backEnd-Spring/src/main/java/com/lucas/request/CourseRequest.java
@@ -4,16 +4,18 @@ import java.util.List;
 
 import org.hibernate.validator.constraints.Length;
 
+import com.lucas.enums.Category;
+import com.lucas.enums.ValueOfEnum;
+
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 public record CourseRequest(
         Long id,
         @NotBlank @NotNull @Length(min = 3, max = 25) String name,
-        @NotNull @Length(max = 25) @Pattern(regexp = "Back-end|Front-end") String category,
+        @NotNull @Length(max = 25) @ValueOfEnum(enumClass = Category.class) String category,
         @NotNull  @NotEmpty @Valid List<LessonRequest> lessons
         ) {
 
--- Crud-backEnd-Spring/src/main/java/com/lucas/response/CourseResponse.java
+++ Crud-backEnd-Spring/src/main/java/com/lucas/response/CourseResponse.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 import org.hibernate.validator.constraints.Length;
 
+import com.lucas.enums.ValueOfEnum;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 public record CourseResponse(
                 Long id,
                 @NotBlank @NotNull @Length(min = 3, max = 25) String name,
-                @NotNull @Length(max = 25) @Pattern(regexp = "Back-end|Front-end") String category,
+                @NotNull @Length(max = 25) String category,
                 @NotNull @NotBlank List<LessonResponse> lessons) {
 }
